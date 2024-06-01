d40aa3ebe8cb0e30ac2a9e889292e5a4b71c14f6
Agape <theaman249@gmail.com>
2024-06-01 18:30:50 +0200

    first push

---
+++ main.py
@@ -0,0 +1,44 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
+
+# tweet = "@MehranShakarami today's cold @ home 😒 https://mehranshakarami.com"
+#tweet = 'Going to commit this crime'
+# tweet = 'I killed a man'
+tweet = 'selling cocaine. It is bad for my clients. But I am making money'
+# tweet = 'So happy for my life'
+
+
+# pre-prcess tweet
+tweet_words = []
+
+for word in tweet.split(' '):
+    if word.startswith('@') and len(word) > 1:
+        word = '@user'
+    
+    elif word.startswith('http'):
+        word = "http"
+    tweet_words.append(word)
+
+tweet_proc = " ".join(tweet_words)
+
+# load model and tokenizer
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+
+labels = ['Negative', 'Neutral', 'Positive']
+
+# sentiment analysis
+encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
+# output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
+output = model(**encoded_tweet)
+
+scores = output[0][0].detach().numpy()
+scores = softmax(scores)
+
+for i in range(len(scores)):
+    
+    l = labels[i]
+    s = scores[i]
+    print(l,s)

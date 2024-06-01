4556fb1b722acbacbeea2981b80a2145d038d54f
Agape <theaman249@gmail.com>
2024-06-01 19:17:45 +0200

    ✅ Edited some data and the main.py

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ from scipy.special import softmax
 flagged_negative_tweets = []
 flagged_neutral_tweets = []
 
+#samples
 # tweet = "@MehranShakarami today's cold @ home 😒 https://mehranshakarami.com"
 #tweet = 'Going to commit this crime'
 # tweet = 'I killed a man'
 tweet = 'selling cocaine. It is bad for my clients. But I am making money'
 # tweet = 'So happy for my life'
 
-
-# pre-prcess tweet
-tweet_words = []
-
-for word in tweet.split(' '):
-    if word.startswith('@') and len(word) > 1:
-        word = '@user'
+def pre_process_data():
+    tweet_words = []
+    for word in tweet.split(' '):
+        if word.startswith('@') and len(word) > 1:
+            word = '@user'
+        
+        elif word.startswith('http'):
+            word = "http"
+        tweet_words.append(word)
     
-    elif word.startswith('http'):
-        word = "http"
-    tweet_words.append(word)
+    return tweet_words
 
-tweet_proc = " ".join(tweet_words)
+tweet_proc = " ".join(pre_process_data)
 
 # load model and tokenizer
 roberta = "cardiffnlp/twitter-roberta-base-sentiment"

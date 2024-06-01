8412f9588dbef29115a82222db083ebda9f20af6
Agape <theaman249@gmail.com>
2024-06-01 21:12:23 +0200

    🚀Code modified to make it more obscure

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ from scipy.special import softmax
 flagged_negative_tweets = []
 flagged_neutral_tweets = []
 
+
+labels = ['Negative', 'Neutral', 'Positive']
+
 #samples
 # tweet = "@MehranShakarami today's cold @ home 😒 https://mehranshakarami.com"
 #tweet = 'Going to commit this crime'
 # tweet = 'I killed a man'
-tweet = 'selling cocaine. It is bad for my clients. But I am making money'
+tweet = ' @John selling cocaine. It is bad for my clients. But I am making money http://drugs.com'
 # tweet = 'So happy for my life'
 
-def pre_process_data():
+
+# Due to the nature of our model, it won't allow @Cars or any http links. Thus, we 
+# must clean up our tweets to only include the @user and http tag. i.e @John = @user and https://drugs.com = http
+# The function takes in a tweet as a parameter
+def pre_process_data(fn_tweet):
     tweet_words = []
     for word in tweet.split(' '):
         if word.startswith('@') and len(word) > 1:
@@ -23,23 +30,28 @@ def pre_process_data():
     
     return tweet_words
 
-tweet_proc = " ".join(pre_process_data)
+tweet_proc = " ".join(pre_process_data(tweet)) #after the tweet has been cleaned. Tweet processed.
+
 
 # load model and tokenizer
+
 roberta = "cardiffnlp/twitter-roberta-base-sentiment"
 
-model = AutoModelForSequenceClassification.from_pretrained(roberta)
-tokenizer = AutoTokenizer.from_pretrained(roberta)
-
-labels = ['Negative', 'Neutral', 'Positive']
+model = AutoModelForSequenceClassification.from_pretrained(roberta) # the actual model
+tokenizer = AutoTokenizer.from_pretrained(roberta) # convert our tweet text into tokens
 
 # sentiment analysis
-encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
+
+encoded_tweet = tokenizer(tweet_proc, return_tensors='pt') # pytorch tensor of the tweet processed. The encoded tweet is a dictionary 
+
 # output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
+
 output = model(**encoded_tweet)
 
-scores = output[0][0].detach().numpy()
-scores = softmax(scores)
+scores = output[0][0].detach().numpy()  # Get the raw scores (logits)
+
+scores = softmax(scores)  # Apply the softmax function to convert logits to probabilities <Negative, Neutral, Positive>
+
 
 for i in range(len(scores)):
     
